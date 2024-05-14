3c4e4e208788136b4df2f2cee764c26c24e43799
James Collier <james.collier@vib.be>
2024-05-14 14:26:36 +0200

    Dodona: average_temp exercise

---
+++ dodona/exercises/average-weather/description/description.en.md
@@ -0,0 +1,29 @@
+Write a function called `average_temperature` that accepts a list of observation dictionaries as input
+and computes the average value for each key over all observations.
+Your function should return an observation with the average values for each key. This is a dictionary
+with the union of all keys in the input dictionaries. You should not make any assumptions about
+which keys (if any) exist in the observation dictionaries.
+
+You can use this template:
+
+```python
+def average_temperature(observations: list[dict[str, float]]) -> dict[str, float]:
+    ...
+```
+
+Here are some example executions. Do not forget to include a docstring and a test function
+called `test_average_temperature` that returns `"Success"` if the `average_temperature`
+function passes all tests.
+
+## Example
+
+```console?lang=python&prompt=>>>
+>>> average_temperature([])
+{}
+>>> average_temperature([{"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1}])
+{"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1}
+>>> average_temperature([{"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1}, {"BEL": 10.2, "RUS": -2.0, "AUS": 37.8, "GLO": 21.2}, {"BEL": 14.7, "RUS": 1.3, "AUS": 18.3, "GLO": 23.6}])
+{"BEL": 10.2, "RUS": -4.5, "AUS": 32.9, "GLO": 22.3}
+>>> average_temperature([{"ABC": 12.3}, {"XYZ": 1.8}])
+{"ABC": 12.3, "XYZ": 1.8}
+```
---
+++ dodona/exercises/average-weather/solution/solution.py
@@ -0,0 +1,45 @@
+def average_temperature(observations: list[dict[str, float]]) -> dict[str, float]:
+    "Compute the average temperature from observations in region keys."
+    sums = {}
+    counts = {}
+    for observation in observations:
+        for region in observation:
+            if region in sums:
+                sums[region] = sums[region] + observation[region]
+            else:
+                sums[region] = observation[region]
+
+            if region in counts:
+                counts[region] = counts[region] + 1
+            else:
+                counts[region] = 1
+
+    average = {}
+    for region in sums:
+        average[region] = sums[region] / counts[region]
+
+    return average
+
+
+def test_average_temperature():
+    "Tests for the average_temperature function."
+    assert average_temperature([]) == {}
+    assert average_temperature([{"ABC": 2.0}, {"XYZ": -2.0}]) == {
+        "ABC": 2.0,
+        "XYZ": -2.0,
+    }
+    assert average_temperature(
+        [{"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1}]
+    ) == {"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1}
+    assert average_temperature(
+        [
+            {"BEL": 5.7, "RUS": -12.8, "AUS": 42.6, "GLO": 22.1},
+            {"BEL": 10.2, "RUS": -2.0, "AUS": 37.8, "GLO": 21.2},
+            {"BEL": 14.7, "RUS": 1.3, "AUS": 18.3, "GLO": 23.6},
+        ]
+    ) == {"BEL": 10.2, "RUS": -4.5, "AUS": 32.9, "GLO": 22.3}
+    assert average_temperature([{}, {"A": 1.5}, {"B", 0.5}, {"A": 2.5}]) == {
+        "A": 2.0,
+        "B": 0.5,
+    }
+    return "Success"

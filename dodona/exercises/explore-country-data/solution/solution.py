397bbcd318815c3db2437539f3293eca65a3d5b1
James Collier <james.collier@vib.be>
2024-05-17 09:37:00 +0200

    Dumb typo in solution

--- dodona/exercises/explore-country-data/solution/solution.py
+++ dodona/exercises/explore-country-data/solution/solution.py
@@ -4,11 +4,11 @@ import csv
 def parse_csv(csv_source) -> list[dict[str, str]]:
     "Parse all rows of data from csv_source emitting CSV formatted data."
     result = []
-    if type(csv_source != str):
+    if type(csv_source) != str:
         # It's a resource then
         source = csv_source.read()
 
-    if type(source == bytes):
+    if type(source) == bytes:
         source = csv_source.decode()
 
     for row in csv.DictReader(source.splitlines()):

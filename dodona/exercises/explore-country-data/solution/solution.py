6debfc8d7d9a794814089162289de6e5281c7a4e
James Collier <james.collier@vib.be>
2024-05-17 09:39:05 +0200

    Another oops

--- dodona/exercises/explore-country-data/solution/solution.py
+++ dodona/exercises/explore-country-data/solution/solution.py
@@ -4,6 +4,7 @@ import csv
 def parse_csv(csv_source) -> list[dict[str, str]]:
     "Parse all rows of data from csv_source emitting CSV formatted data."
     result = []
+    source = csv_source
     if type(csv_source) != str:
         # It's a resource then
         source = csv_source.read()

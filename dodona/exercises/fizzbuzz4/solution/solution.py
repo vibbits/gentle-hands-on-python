06f8ea7206833aa3d3aaf790e267a5cb9a557a29
James Collier <james.collier@vib.be>
2024-10-14 10:53:25 +0200

    More strictly evaluate Fizzbuzz4

--- dodona/exercises/fizzbuzz4/solution/solution.py
+++ dodona/exercises/fizzbuzz4/solution/solution.py
@@ -7,5 +7,10 @@ def fizzbuzz(counted: list[int]) -> list[int | str]:
 
 def test_fizzbuzz():
     "Tests for the fizzbuzz function."
+    assert fizzbuzz([]) == []
     assert fizzbuzz([1, 2, 3, 4, 5]) == [1, 2, "Fizz", 4, "Buzz"]
+    assert fizzbuzz(['a', 'b', 'c', 'd', 'e']) == ['a', 'b', "Fizz", 'd', "Buzz"]
     return "Success"
+
+if __name__ == "__main__":
+    test_fizzbuzz()

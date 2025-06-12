ff0cf231d110f91a975828269b3dc0a209204d4d
James Collier <james.collier@vib.be>
2025-06-12 14:14:40 +0200

    Add test case to the length of highest quality subsequence exercise

--- dodona/exercises/highest-quility-subsequence/solution/solution.py
+++ dodona/exercises/highest-quility-subsequence/solution/solution.py
@@ -1,5 +1,6 @@
 import typing
 import io
+import operator
 from pathlib import Path
 
 def best_subsequence_length(data: typing.TextIO) -> list[int]:
@@ -10,30 +11,32 @@ def best_subsequence_length(data: typing.TextIO) -> list[int]:
     if len(raw[1]) != len(raw[3]):
         raise ValueError("Quality scores do not match")
     
-    highest = 0
-    length = 0
     minimum = int.from_bytes(b'!')
     maximum = int.from_bytes(b'~')
+    scores = []
     for score_character in raw[3]:
         score = int.from_bytes(score_character.encode())
         if score < minimum:
             raise ValueError("Quality score too low")
         if score > maximum:
             raise ValueError("Quality score too high")
-        if score == highest:
-            length += 1
-        elif score > highest:
-            length = 1
-            highest = score
 
-    return [highest, length]
+        if len(scores) == 0:
+            scores.append([score, 1])
+        elif scores[-1][0] == score:
+            scores[-1][1] += 1
+        else:
+            scores.append([score, 1])
 
+    return sorted(scores, key=operator.itemgetter(1), reverse=True)[0]
 
 
 def test_best_subsequence_length():
     """Test best_subsequence_length function."""
     resource = io.StringIO("@SEQ\nATGCC\n+\n*///%")
     assert [47, 3] == best_subsequence_length(resource)
+    resource = io.StringIO("@SEQ\nATAATGATTC\n+\n*///%%////")
+    assert [47, 4] == best_subsequence_length(resource)
     resource = io.StringIO("@SEQ\nATGC\n+\nA2YY")
     assert [89, 2] == best_subsequence_length(resource)
 
